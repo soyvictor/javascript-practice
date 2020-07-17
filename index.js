ec2321642c6798f60090fe84c1610281d1d189e8
Victor Chiang <vgchiang@gmail.com>
2020-07-16 22:10:42 -0500

    practiced javascript

--- index.js
+++ index.js
@@ -184,7 +184,7 @@ alert("hello user!");
 
 // P.S. In this task the function should support only natural values of n: integers up from 1.
 
-function pow(x,n) {
+/*function pow(x,n) {
   let result = x;
 
   for (i = 1; i < n; i++ ) {
@@ -204,6 +204,173 @@ if (n < 1) {
 else {
   alert(pow(x,n));
 }
+
+
+// Arrow functions
+
+Rewrite with arrow functions
+Replace Function Expressions with arrow functions in the code below:
+
+function ask(question, yes, no) {
+  if (confirm(question)) yes()
+  else no();
+}
+
+ask(
+  "Do you agree?",
+  function() { alert("You agreed."); },
+  function() { alert("You canceled the execution."); }
+);
+
+
+let ask = (question, yes, no) => {
+  if (confirm(question)) yes()
+    else no();
+}
+
+let ask = () => {
+  "Do you agree?",
+  () => alert("you agreed."),
+  () => alert("You cancelled the execution.")
+}
+
+
+What’s wrong with the code style below?
+
+
+function pow(x, n) {
+  let result = 1;
+
+  for(let i = 0; i < n; i++) {
+    result *= x;
+  }
+
+  return result;
+}
+
+let x = prompt("x?", '');
+let n=prompt("n?",'');
+
+if (n <= 0) {
+  alert(`Power ${n} is not supported,
+   please enter an integer number greater than zero`);
+} else {
+  alert( pow(x,n) );
+}
+
+/* This is
+a multiline comment
+wowww. */
+
+// This is a single line comment.
+
+/*
+Hello, object
+importance: 5
+Write the code, one line for each action:
+
+Create an empty object user.
+Add the property name with the value John.
+Add the property surname with the value Smith.
+Change the value of the name to Pete.
+Remove the property name from the object.
+
+let user = {};
+user.name = "John";
+user.surname = "Smith";
+user.name = "Pete";
+delete user.name;
+
+
+Write the function isEmpty(obj) which returns true if the object has no properties, false otherwise.
+
+Should work like that:
+
+let schedule = {};
+
+alert( isEmpty(schedule) ); // true
+
+schedule["8:30"] = "get up";
+
+alert( isEmpty(schedule) ); // false
+
+function isEmpty(obj) {
+  for (let key in obj) {
+    return false;
+  }
+  return true;
+}
+/**/
+
+
+/*Sum object properties
+importance: 5
+We have an object storing salaries of our team:
+
+
+Write the code to sum all salaries and store in the variable sum. Should be 390 in the example above.
+
+If salaries is empty, then the result must be 0.*/
+
+
+/*let salaries = {
+  John: 100,
+  Ann: 160,
+  Pete: 130
+}
+
+function sumSalaries(obj) {
+
+  let sum = 0
+
+  for (let key in obj) {
+    sum += obj[key];
+  }
+
+  if (sum == 0) {
+    return 0;
+  } else {
+    return sum;
+  }
+}
+
+console.log( sumSalaries(salaries) );*/
+
+
+Multiply numeric properties by 2
+importance: 3
+Create a function multiplyNumeric(obj) that multiplies all numeric properties of obj by 2.
+
+For instance:
+
+// before the call
+let menu = {
+  width: 200,
+  height: 300,
+  title: "My menu"
+};
+
+multiplyNumeric(menu);
+
+// after the call
+menu = {
+  width: 400,
+  height: 600,
+  title: "My menu"
+};
+Please note that multiplyNumeric does not need to return anything. It should modify the object in-place.
+
+P.S. Use typeof to check for a number here.
+
+
+function multiplyNumeric(obj) {
+  for ( let key in obj) {
+    if (typeof obj[key] == 'number' {
+      ojb[key] *= 2
+    }
+  }
+}
+
 
 
 
